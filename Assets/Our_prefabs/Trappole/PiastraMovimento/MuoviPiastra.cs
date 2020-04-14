c09d93a4be8e189f6187e6c9131837384e1218ec
cosimob28 <43493719+cosimob28@users.noreply.github.com>
2020-04-14 23:16:10 +0200

    aggiornamenti piastra movimento

---
+++ Assets/Our_prefabs/Trappole/PiastraMovimento/MuoviPiastra.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuoviPiastra : MonoBehaviour
+{
+    int speedAmt;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.z >= -22.62)
+        {
+            speedAmt = -10;
+        }
+        if(transform.position.z <= -39.58)
+        {
+            speedAmt = 10;
+        }
+        transform.Translate(0, 0, speedAmt * Time.deltaTime);
+    }
+}
