7164e4ea3b933687ab44bbbfd86b9a9048dc2892
Alex <bacco.a10@gmail.com>
2020-04-18 00:24:23 +0200

    Scene Credits and Main Menu
    ---

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+
+    public void ChangeToTutorial() {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void ChangeToCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitApp() {
+        Application.Quit();
+    }
+}
