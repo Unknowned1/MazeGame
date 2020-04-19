60b03cb901572cfc4bf93d4627050ca1d619c6b0
cosimob28 <43493719+cosimob28@users.noreply.github.com>
2020-04-19 23:23:20 +0200

    Aggiornamenti vari

--- Assets/Script/CollisionWithPendulum.cs
+++ Assets/Script/CollisionWithPendulum.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionWithPendulum : MonoBehaviour
 {
-
+    private bool eliminaSottraiTimer = false;
+    public GameObject panel;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +14,18 @@ public class CollisionWithPendulum : MonoBehaviour
         {
             // diminuisce il tempo
             GameObject.Find("timer").GetComponentInChildren<Timer>().time -= 10;
+            GameObject.Find("sottraiTimer").GetComponentInChildren<Text>().text = "-10";
+            StartCoroutine(ExampleCoroutine());
+            GameObject.Find("sottraiTimer").GetComponentInChildren<Text>().text = "";
         }
+    }
+
+   
+
+    IEnumerator ExampleCoroutine()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+
     }
 }
