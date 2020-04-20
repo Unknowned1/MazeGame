3890733613f8fab405705ca75ffe24fe3afb19f8
Alex <bacco.a10@gmail.com>
2020-04-21 01:10:01 +0200

    final steps (1)
    ---

--- Assets/Script/EndTrigger.cs
+++ Assets/Script/EndTrigger.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 
@@ -7,6 +8,7 @@ public class EndTrigger : MonoBehaviour
     public GameObject panel_basic_ui;
     public GameObject level_completed_ui;
     public GameObject game_over_panel;
+    public GameObject pause_panel;
     public GameObject player;
 
     private void Update()
@@ -17,6 +19,29 @@ public class EndTrigger : MonoBehaviour
             level_completed_ui.SetActive(false);
             GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("esc pressed ");
+            pause_panel.SetActive(true);
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+            //PauseGame();
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pause_panel.SetActive(false);
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
+            Time.timeScale = 1;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //added action to set te pause menu back to inactive after resume button is pressed
+            pause_panel.SetActive(false);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,5 +55,19 @@ public class EndTrigger : MonoBehaviour
         }
     }
 
+    
 
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pause_panel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pause_panel.SetActive(false);
+        //enable the scripts again
+    }
 }
