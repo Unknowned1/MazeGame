7aa801d75fa1597e619e2118c81342a351834639
Alex <bacco.a10@gmail.com>
2020-04-21 22:27:55 +0200

    intro scene added
    ---

---
+++ Assets/Script/ChangeFromIntroToMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFromIntroToMenu : MonoBehaviour
+{
+    bool loading_started = false;
+    float seconds_left = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayLoadLevel(5));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DelayLoadLevel(float seconds) {
+        seconds_left = seconds;
+        loading_started = true;
+        do
+        {
+            yield return new WaitForSeconds(1);
+        } while (--seconds_left > 0);
+    }
+}
---
+++ Assets/Script/ImageToFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageToFade : MonoBehaviour
+{
+    public Image img;
+    Color color_img;
+    public float value_of_fade = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        color_img = img.GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        color_img.a -= value_of_fade * Time.deltaTime;
+        img.color = color_img;
+    }
+}
