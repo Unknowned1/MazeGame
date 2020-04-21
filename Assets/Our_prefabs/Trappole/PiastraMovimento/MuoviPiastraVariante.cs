5bdb0051815f04f4ad43346f5dbbccb32742b9b9
cosimob28 <43493719+cosimob28@users.noreply.github.com>
2020-04-21 22:51:18 +0200

    Aggiunta prima scena

---
+++ Assets/Our_prefabs/Trappole/PiastraMovimento/MuoviPiastraVariante.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuoviPiastraVariante : MonoBehaviour
+{
+    public int speedAmt;
+    public float minimo, massimo;
+    private int speedAmt1;
+
+    void Start()
+    {
+        speedAmt1 = speedAmt;
+    }
+
+    void Update()
+    {
+        if (transform.position.x <= minimo)
+        {
+            speedAmt1 = -speedAmt;
+        }
+        if (transform.position.x >= massimo)
+        {
+            speedAmt1 = speedAmt;
+        }
+        transform.Translate(speedAmt1 * Time.deltaTime, 0, 0);
+    
+    }
+}
